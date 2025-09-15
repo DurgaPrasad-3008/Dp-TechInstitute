cd053031ac66c3831378d7faa8c0f1106e71e27e
Puttala Durga Prasad <durgaprasadmudhiraj1267@gmail.com>
2025-09-15 21:52:46 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -79,7 +79,7 @@ const Navbar = () => {
               onClick={handleLoginClick}
               className="flex items-center text-gray-300 hover:text-amber-400 transition-colors duration-200"
             >
-              <span className="text-xl mr-2">💕</span></button>
+              <span className="text-xl mr-2">💕</span>Boss</button>
           </div>
 
           {/* Mobile Menu Toggle */}
